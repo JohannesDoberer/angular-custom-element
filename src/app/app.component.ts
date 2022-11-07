989fa5ad5bb01737370591b38eb4d86adf027d1c
Doberer, Johannes <johannes.doberer@sap.com>
2022-11-07 14:27:33 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { LuigiElement } from '@luigi-project/client/luigi-element'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent extends LuigiElement {
   title = 'my-element';
+  constructor() {
+    super();
+    console.log('FT from angular webcomponent', this.LuigiClient.getActiveFeatureToggles());
+  }
 }
